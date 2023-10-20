ff9a2595b2fd2dabcdcf52a8122ea836099039dd
manoj-65 <manoj.y@testyantra.in>
2023-10-20 13:09:40 +0530

    user model modified

--- src/main/java/com/ty/ams/dao/UserDao.java
+++ src/main/java/com/ty/ams/dao/UserDao.java
@@ -1,5 +1,8 @@
 package com.ty.ams.dao;
 
+import com.ty.ams.entity.User;
+
 public interface UserDao {
 
+	User findUserById(long userId);
 }
--- src/main/java/com/ty/ams/daoimp/UserDaoImp.java
+++ src/main/java/com/ty/ams/daoimp/UserDaoImp.java
@@ -1,7 +1,14 @@
 package com.ty.ams.daoimp;
 
 import com.ty.ams.dao.UserDao;
+import com.ty.ams.entity.User;
 
 public class UserDaoImp implements UserDao {
+
+	@Override
+	public User findUserById(long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
