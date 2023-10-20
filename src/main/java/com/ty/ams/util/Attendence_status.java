a0ee72a2c6e91acab77df60a7ea7fe66332cc42d
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2023-10-20 14:36:37 +0530

    All the entitys are created and along with the enums

---
+++ src/main/java/com/ty/ams/util/Attendence_status.java
@@ -0,0 +1,6 @@
+package com.ty.ams.util;
+
+public enum Attendence_status {
+	PRESENT, ABSENT, PRESENT_WFH, ABSENT_WFH, WFH, COLLEGE_TRAINING
+}
+
---
+++ src/main/java/com/ty/ams/util/BatchStatus.java
@@ -0,0 +1,5 @@
+package com.ty.ams.util;
+
+public enum BatchStatus {
+	yet_to_assign, assigned, completed
+}
