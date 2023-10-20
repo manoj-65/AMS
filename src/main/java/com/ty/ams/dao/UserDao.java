48ca1bb71a531d5c5e4cdf49db9ab7dce25fe44b
gaganvdeveloper <gaganvdeveloper@gmail.com>
2023-10-20 14:16:36 +0530

    User Udate Method is Completd

--- src/main/java/com/ty/ams/dao/UserDao.java
+++ src/main/java/com/ty/ams/dao/UserDao.java
@@ -1,10 +1,14 @@
 package com.ty.ams.dao;
 
+import java.util.Optional;
+
 import com.ty.ams.entity.User;
 
 public interface UserDao {
 
-	User findUserById(long userId);
+	Optional<User> findUserById(int userId);
 
 	User saveUser(User user);
+	
+	User updateUser(User user);
 }
--- src/main/java/com/ty/ams/repository/UserRepository.java
+++ src/main/java/com/ty/ams/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.ty.ams.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ty.ams.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
 
 }
