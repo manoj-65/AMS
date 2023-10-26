120bb105185ce3869eb1eeee6ef81cd7e4069925
gaganvdeveloper <gaganvdeveloper@gmail.com>
2023-10-26 13:01:59 +0530

    Swagger doc implementation Provided for user

---
+++ src/main/java/com/ty/ams/config/SwaggerConfiguration.java
@@ -0,0 +1,19 @@
+package com.ty.ams.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfiguration {
+	
+	
+	
+}
