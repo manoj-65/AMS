e4448ed52240713018b976a46454a04fb9624a03
unknown <sanminnu123@gamil.com>
2023-11-20 11:51:07 +0530

    commiting changes done image

--- src/main/java/com/ty/ams/controller/ImageController.java
+++ src/main/java/com/ty/ams/controller/ImageController.java
@@ -1,11 +1,13 @@
 package com.ty.ams.controller;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,19 +16,28 @@ import org.springframework.web.multipart.MultipartFile;
 import com.ty.ams.entity.Image;
 import com.ty.ams.repository.ImageRepository;
 import com.ty.ams.responsestructure.ResponseStructure;
+import com.ty.ams.service.ImageService;
 
 @RestController
 public class ImageController {
-       @Autowired
-	  ImageRepository imageRepository;
-      
-       
-       @PostMapping("/saveImage")
-       public ResponseEntity<ResponseStructure<Image>> uploadImage(@RequestParam MultipartFile file) throws IOException{
-    	 System.out.println("dfgh");
-    	   Image m= new Image();
-			m.setImage(file.getBytes());
-    	Image image =imageRepository.save(m);
-    	   return new ResponseEntity<ResponseStructure<Image>>(new ResponseStructure<Image>(), HttpStatus.CREATED);
-       }
+	
+	@Autowired
+	ImageService imageService;
+
+	@PostMapping("/saveImage")
+	public ResponseEntity<ResponseStructure<Image>> uploadImage(@RequestParam MultipartFile file) throws IOException {
+		Image image = new Image();
+		image.setImage(file.getBytes());
+//		Image image = imageRepository.save(m);
+		return imageService.saveImage(image);
+	}
+
+	@GetMapping(value = "/getimage/{id}")
+	public ResponseEntity<ResponseStructure<Image>> getimageInfoById(@PathVariable int id) {
+		ResponseEntity<ResponseStructure<Image>> image = imageService.getImageById(id);
+
+		return image;
+
+	}
 }
+
