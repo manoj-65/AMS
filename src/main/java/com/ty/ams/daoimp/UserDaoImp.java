aba6f07dfdbc002ea9e55e8b8bd7559304aca228
Shubhamkumardeveloper <shubhamkrsam1809@gmail.com>
2023-11-21 15:02:26 +0530

    user controller modified

--- src/main/java/com/ty/ams/daoimp/UserDaoImp.java
+++ src/main/java/com/ty/ams/daoimp/UserDaoImp.java
@@ -1,15 +1,18 @@
 package com.ty.ams.daoimp;
 
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Locale.Category;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.stereotype.Repository;
 
 import com.ty.ams.dao.UserDao;
 import com.ty.ams.entity.User;
 import com.ty.ams.repository.UserRepository;
+import com.ty.ams.util.UserCategory;
 import com.ty.ams.util.UserRole;
 import com.ty.ams.util.UserStatus;
 
@@ -19,17 +22,18 @@ public class UserDaoImp implements UserDao {
 	@Autowired
 	private UserRepository userRepository;
 
-
 	@Override
 	public User saveUser(User user) {
+		// TODO Auto-generated method stub
 		return userRepository.save(user);
 	}
 
 	@Override
 	public User updateUser(User user) {
+		// TODO Auto-generated method stub
 		return userRepository.save(user);
 	}
-	
+
 	@Override
 	public Optional<User> findUserById(int userId) {
 		return userRepository.findById(userId);
@@ -38,6 +42,7 @@ public class UserDaoImp implements UserDao {
 	@Override
 	public void deleteUserById(int id) {
 		userRepository.deleteById(id);
+
 	}
 
 	@Override
@@ -47,43 +52,136 @@ public class UserDaoImp implements UserDao {
 
 	@Override
 	public List<User> findUserByRole(UserRole role) {
+		// TODO Auto-generated method stub
 		return userRepository.findByUserRole(role);
 	}
 
 	@Override
-	public List<User> findUserByCategory(Category category) {
+	public List<User> findUserByCategory(UserCategory category) {
+		// TODO Auto-generated method stub
 		return userRepository.findByUserCategory(category);
 	}
 
 	@Override
 	public List<User> findAllUsers() {
+		// TODO Auto-generated method stub
 		return userRepository.findAll();
 	}
 
 	@Override
 	public Optional<User> findUserByPhoneNumber(long phone) {
+		// TODO Auto-generated method stub
 		return userRepository.findByPhone(phone);
 	}
 
 	@Override
 	public Optional<User> findUserByEmail(String email) {
+		// TODO Auto-generated method stub
 		return userRepository.findByEmail(email);
 	}
 
 	@Override
 	public List<User> findUserByStatus(UserStatus status) {
+		// TODO Auto-generated method stub
 		return userRepository.findByUserStatus(status);
 	}
 
 	@Override
 	public Optional<User> findUserByEmailAndPassword(String email, String password) {
+		// TODO Auto-generated method stub
 		return userRepository.findByEmailAndPassword(email, password);
 	}
 
+	@Override
+	public User findUserByPhoneAndPassword(long phone, String password) {
+		// TODO Auto-generated method stub
+		return userRepository.findByPhoneAndPassword(phone, password).get();
+	}
 
 	@Override
-	public Optional<User> findUserByPhoneAndPassword(long phone, String password) {
-		return userRepository.findByPhoneAndPassword(phone, password);
+	public List<User> findAllActiveUsers() {
+		// TODO Auto-generated method stub
+		return userRepository.findAllInActiveUsers();
 	}
+
+	@Override
+	public List<User> findAllInActiveUsers() {
+		// TODO Auto-generated method stub
+		return userRepository.findAllInActiveUsers();
+	}
+
+
+	
+	
+	
+	
+	
+	
+
+//	@Override
+//	public User saveUser(User user) {
+//		return userRepository.save(user);
+//	}
+//
+//	@Override
+//	public User updateUser(User user) {
+//		return userRepository.save(user);
+//	}
+//	
+//	@Override
+//	public Optional<User> findUserById(int userId) {
+//		return userRepository.findById(userId);
+//	}
+//
+//	@Override
+//	public void deleteUserById(int id) {
+//		userRepository.deleteById(id);
+//	}
+//
+//	@Override
+//	public Optional<User> findUserByEmpId(String empId) {
+//		return userRepository.findByEmpId(empId);
+//	}
+//
+//	@Override
+//	public List<User> findUserByRole(UserRole role) {
+//		return userRepository.findByUserRole(role);
+//	}
+//
+//	@Override
+//	public List<User> findUserByCategory(Category category) {
+//		return userRepository.findByUserCategory(category);
+//	}
+//
+//	@Override
+//	public List<User> findAllUsers() {
+//		return userRepository.findAll();
+//	}
+//
+//	@Override
+//	public Optional<User> findUserByPhoneNumber(long phone) {
+//		return userRepository.findByPhone(phone);
+//	}
+//
+//	@Override
+//	public Optional<User> findUserByEmail(String email) {
+//		return userRepository.findByEmail(email);
+//	}
+//
+//	@Override
+//	public List<User> findUserByStatus(UserStatus status) {
+//		return userRepository.findByUserStatus(status);
+//	}
+//
+//	@Override
+//	public Optional<User> findUserByEmailAndPassword(String email, String password) {
+//		return userRepository.findByEmailAndPassword(email, password);
+//	}
+//
+//
+//	@Override
+//	public Optional<User> findUserByPhoneAndPassword(long phone, String password) {
+//		return userRepository.findByPhoneAndPassword(phone, password);
+//	}
 
 }
