7ed12e6708bd7a0b54dc2873787b868fafb33ad2
PavansaiPodichervu <podichervupavansai@gmail.com>
2023-11-21 15:06:43 +0530

    user controller modified

--- src/main/java/com/ty/ams/controller/UserController.java
+++ src/main/java/com/ty/ams/controller/UserController.java
@@ -2,7 +2,6 @@ package com.ty.ams.controller;
 
 import java.time.LocalTime;
 import java.util.List;
-import java.util.Locale.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
