9f409044a643c0745b6466069c3454dc47f51d28
ma-noj <manojy0505@gmail.com>
2024-01-19 13:15:20 +0530

    User Model Modified

--- src/main/java/com/ty/ams/service/UserService.java
+++ src/main/java/com/ty/ams/service/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 
+import com.ty.ams.dto.UserDto;
 import com.ty.ams.entity.User;
 import com.ty.ams.responsestructure.ResponseStructure;
 import com.ty.ams.util.UserCategory;
@@ -42,40 +43,41 @@ public interface UserService {
 //	ResponseEntity<ResponseStructure<User>> findUserByPhoneAndPassword(long phone, String password);
 //
 //	ResponseEntity<ResponseStructure<User>> setUserStatusToInAcativeByUserId(int userId);
-	
-	
+
 	ResponseEntity<ResponseStructure<User>> saveUser(User user);
-	
+
 	ResponseEntity<ResponseStructure<User>> updateUser(User user);
-	
+
 	ResponseEntity<ResponseStructure<User>> findUserById(int id);
-	
-	ResponseEntity<ResponseStructure<User>>findUserByEmpId(String id);
-	
-	ResponseEntity<ResponseStructure<User>>findUserByPhone(long phone);
-	
-	ResponseEntity<ResponseStructure<User>>findUserByEmail(String email);
-	
+
+	ResponseEntity<ResponseStructure<User>> findUserByEmpId(String id);
+
+	ResponseEntity<ResponseStructure<User>> findUserByPhone(long phone);
+
+	ResponseEntity<ResponseStructure<User>> findUserByEmail(String email);
+
 	ResponseEntity<ResponseStructure<String>> deleteUserById(int id);
-	
+
 	ResponseEntity<ResponseStructure<User>> findUserByEmailAndPassword(String email, String password);
-	
+
 	ResponseEntity<ResponseStructure<User>> setUserStatusToInActive(int id);
-	
+
 	ResponseEntity<ResponseStructure<List<User>>> findAllUsers();
 
 	ResponseEntity<ResponseStructure<List<User>>> findAllActiveUsers();
 
 	ResponseEntity<ResponseStructure<List<User>>> findAllInActiveUsers();
-	
+
 	ResponseEntity<ResponseStructure<List<User>>> findUserByRole(UserRole role);
-	
+
 	ResponseEntity<ResponseStructure<List<User>>> findUserByCategory(UserCategory category);
-	
+
 	ResponseEntity<ResponseStructure<List<User>>> findUserByStatus(UserStatus status);
-	
+
 	ResponseEntity<ResponseStructure<List<LocalTime>>> findBatchTimingsOfUser(int userId);
-	
+
 	ResponseEntity<ResponseStructure<User>> setUserStatusToInAcativeByUserId(int userId);
+
+	ResponseEntity<ResponseStructure<List<UserDto>>> findAllTrainersToAssiginBatch();
 
 }
